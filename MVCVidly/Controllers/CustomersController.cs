8bafabf9ddd06aae64f78f26924f0826ef884a24
Leo Ris <ristanovski@hotmail.com>
2022-01-08 23:36:03 +0100

    Adding Discount Rate table header and adding values inside Customers table.

--- MVCVidly/Controllers/CustomersController.cs
+++ MVCVidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,16 +10,26 @@ namespace MVCVidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -26,15 +37,6 @@ namespace MVCVidly.Controllers
             }
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Alan Shore"},
-                new Customer { Id = 2, Name = "Denny Crane"}
-            };
         }
     }
 }
