5467019c4723d76699b9e2d78367497a4a6896e0
Leo Ris <ristanovski@hotmail.com>
2022-01-09 10:25:06 +0100

    Renaming Discount Rate to Membership Type and adding the rules for membership.

---
+++ MVCVidly/Migrations/202201082244364_NameColumnToMembershipTypeTable.cs
@@ -0,0 +1,18 @@
+namespace MVCVidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NameColumnToMembershipTypeTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
