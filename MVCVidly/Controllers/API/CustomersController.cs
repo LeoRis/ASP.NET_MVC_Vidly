1050663a73ce16e63cd0bcbf432cfd0a322f97fc
Leo Ris <ristanovski@hotmail.com>
2022-01-10 17:29:34 +0100

    API to get 1 customer by its ID.

--- MVCVidly/Controllers/API/CustomersController.cs
+++ MVCVidly/Controllers/API/CustomersController.cs
@@ -4,10 +4,35 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MVCVidly.Models;
 
 namespace MVCVidly.Controllers.API
 {
     public class CustomersController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/customers/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customer;
+        }
     }
 }
